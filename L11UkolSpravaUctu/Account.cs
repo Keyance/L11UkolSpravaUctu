f33cafbfd7410ec4155cc8a0291fd11c93b573e0
Keyance <klara.konopacova@seznam.cz>
2024-01-16 12:16:29 +0100

    Přidejte soubory projektu.

---
+++ L11UkolSpravaUctu/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace L11UkolSpravaUctu
+{
+    internal class Account
+    {
+        private string _accountNumber;
+        public string AccountNumber 
+        {
+            get
+            {
+                return _accountNumber;
+            }
+            set
+            {
+                _accountNumber = value;
+            }
+        }
+
+        private string _ownerName;
+        public string OwnerName 
+        {
+            get
+            {
+                return _ownerName;
+            }
+            set
+            {
+                _ownerName = value;
+            }
+        }
+
+        private decimal _balance;
+        public decimal Balance 
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+        public void Deposit (decimal depositMoney)
+        {
+            Balance = Balance + depositMoney;
+        }
+
+        public void Withdraw (decimal amount)
+        {
+            if (amount < _balance)
+            {
+                _balance -= amount;
+            } else
+            { Console.WriteLine("Na účtu není dost peněz na tento výběr."); }
+        }
+    }
+}
---
+++ L11UkolSpravaUctu/Program.cs
@@ -0,0 +1,6 @@
+﻿using L11UkolSpravaUctu;
+
+Account account = new Account();
+account.Deposit(1354);
+account.OwnerName = "Mojmír";
+account.Withdraw(2544);
