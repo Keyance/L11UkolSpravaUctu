21d771a9a7548f49e40b47821df442db6ed3d7f9
Keyance <klara.konopacova@seznam.cz>
2024-01-19 11:20:49 +0100

    obklopeno try-catch v main

--- L11UkolSpravaUctu/Program.cs
+++ L11UkolSpravaUctu/Program.cs
@@ -1,6 +1,26 @@
 ﻿using L11UkolSpravaUctu;
 
-Account account = new Account();
-account.Deposit(1354);
-account.OwnerName = "Mojmír";
-account.Withdraw(2547);
+
+try
+{
+	Account account = new Account();
+    account.Deposit(1354);
+    account.OwnerName = "Mojmír";
+    account.Withdraw(2547);
+
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
