ef5f144286d4d6073c095502de124e873a6a1cec
Keyance <klara.konopacova@seznam.cz>
2024-01-19 11:23:23 +0100

    přidány další testy v main

--- L11UkolSpravaUctu/Program.cs
+++ L11UkolSpravaUctu/Program.cs
@@ -1,12 +1,34 @@
 ﻿using L11UkolSpravaUctu;
-
+	Account account = new Account();
 
 try
 {
-	Account account = new Account();
     account.Deposit(1354);
     account.OwnerName = "Mojmír";
     account.Withdraw(2547);
+    account.Deposit(-157);
+    account.Balance = -4688;
+
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+
+try
+{
+    account.Balance = -4688;
 
 }
 catch (ArgumentOutOfRangeException e)
